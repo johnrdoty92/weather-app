87287f65d51524c0c6fb08754caa990cf4612623
John Doty <johnrdoty92@gmail.com>
2021-05-23 17:51:15 -0400

    chose css layout set up fetch promise

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
 const submit = document.querySelector('#submit');
 const search = document.querySelector('#search');
 const form = document.forms["search-field"];
-const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let units = 'imperial';
 
+//Takes forecast and displays it to the page
+let displayData = (data) => {
+    let display = document.querySelector('#weather-display');
+    let summary = document.querySelector('#summary');
+    let weekForecast = document.querySelector('#week-forecast');
+    console.log(data);
+};
+
+//Fetch request function for event listener
 let submitRequest = (event) => {
     event.preventDefault();
-    console.log(search.value)
-    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${search.value}&cnt=5&appid=${KEYS.API_KEY}`
-    fetch(url)
-        .then(response => {
-            console.log(response);
-            return response.json();
-        })
-        //Modify this section, it only currently displays a weekday
-        .then(data => {
-            console.log(data.list);
-            let currentDay = new Date(data.list[0].dt_txt.slice(0,10)).getDay(); //the index specifies the weekday
-            console.log(daysOfWeek[currentDay]);
+    console.log(search.value);
+    
+    let currentURL = `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&appid=${KEYS.API_KEY}&units=${units}`
+    let fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${search.value}&cnt=5&appid=${KEYS.API_KEY}&units=${units}`;
+
+    //Fetch Five Day forecast
+    let fiveDay = fetch(fiveDayURL)
+        .then(res => {
+            return res.json();
         })
         .catch(err => {
-            console.log("Request Failed");
-            console.log(err)
+            console.log(`An error occurred: ${err}`);
         });
+    
+    //Fetch current weather
+    let current = fetch(currentURL)
+        .then(res => {
+            return res.json();
+        })
+        .catch(err => {
+            console.log(`An error occurred: ${err}`);
+        });
+
+    //Once current and 5 day weather data is returned:
+    Promise.all([current, fiveDay])
+    .then(data => {
+        displayData(data);
+    })
+    .catch(err => {
+        console.log(`An error occurred: ${err}`);
+    })
 }
 
+//Event Listeners
 form.addEventListener('submit', submitRequest)
-
-let display = document.querySelector('#weather-display');
-let summary = document.querySelector('#summary');
-let weekForecast = document.querySelector('#week-forecast');
